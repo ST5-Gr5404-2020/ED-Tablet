1e66920ff4da07790ba5825f58f6e7cdb61f909a
lleuch18 <61183192+lleuch18@users.noreply.github.com>
2020-11-29 18:17:48 +0100

    loginServer password verify iteration

--- src/main/java/ED/Tablet/model/loginServerConn.java
+++ src/main/java/ED/Tablet/model/loginServerConn.java
@@ -3,6 +3,15 @@ package ED.Tablet;
 public class loginServerConn extends dbConn {
     
     public void validataLogin(String personnelID, String password) {
-        
+		Boolean verifyLogin = false;
+		
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT " + password + "FROM loginServer WHERE ID = " + personnelID);
+
+		if(rs.getString("password") = password)
+		{
+			verifyLogin = true;
+		}
+
     }
 }
