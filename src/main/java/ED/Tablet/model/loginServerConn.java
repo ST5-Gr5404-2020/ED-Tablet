da05e4a3df7ea519849bd645b796c43a1b341e98
lleuch18 <61183192+lleuch18@users.noreply.github.com>
2020-11-30 08:12:23 +0100

    Added connection to DB for loginServer

--- src/main/java/ED/Tablet/model/loginServerConn.java
+++ src/main/java/ED/Tablet/model/loginServerConn.java
@@ -1,14 +1,20 @@
 package ED.Tablet.model;
 
+import java.sql.Connection;
+
 public class loginServerConn extends dbConn {
     
     public void validataLogin(String personnelID, String password) {
 		Boolean verifyLogin = false;
 		
-		Statement stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT " + password + "FROM loginServer WHERE ID = " + personnelID);
 
-		if(rs.getString("password") = password)
+		Connection conn = DriverManager.getConnection("loginServer URL"); //Connecter til databaseservreen
+			conn.setCatalog("loginServer"); //Connecter til loginServer databasen
+
+		Statement stmt = conn.createStatement(); //laver statement object
+		ResultSet rs = stmt.executeQuery("SELECT " + password + "FROM loginServer WHERE ID = " + personnelID); //henter password til tilhørende ID
+
+		if(rs.getString("password") = password) //sammenligner indtastede password med det fra loginServer databasen
 		{
 			verifyLogin = true;
 		}
