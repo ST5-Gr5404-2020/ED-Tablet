a5c85c8a484d355ac84b3b07efea790946b549a1
Nicklas <Nfleis16@student.aau.dk>
2020-12-01 09:15:28 +0100

    push

--- src/main/java/ED/Tablet/model/dbConn.java
+++ src/main/java/ED/Tablet/model/dbConn.java
@@ -37,7 +37,7 @@ public class dbConn {
         
     }
 
-    protected void executeQuery(String sqlStatement){
+    protected void executeQuery(String sqlStatement, String PK){
 
         Connection myDB = connectToDB(host, DBPassword, username);
         
@@ -48,7 +48,7 @@ public class dbConn {
             try {
                 
                 stmt = myDB.createStatement();
-                rs = stmt.executeQuery(sqlStatement);
+                rs = stmt.executeQuery(sqlStatement+PK);
             }
             catch (SQLException ex){
                 // handle any errors
