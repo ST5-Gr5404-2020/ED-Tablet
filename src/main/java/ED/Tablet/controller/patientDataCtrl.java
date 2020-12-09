5b96feeba1db1bac56c565ff2cdcad649d4e32ff
Nicklas <Nfleis16@student.aau.dk>
2020-12-09 16:41:15 +0100

    efefsafe

--- src/main/java/ED/Tablet/controller/patientDataCtrl.java
+++ src/main/java/ED/Tablet/controller/patientDataCtrl.java
@@ -84,7 +84,7 @@ public class patientDataCtrl {
 
     @FXML
     private void initialize() {
-        // Initialize the person table with the two columns.
+        // Initialize the med table with the two columns.
         medNameColumn.setCellValueFactory(cellData -> cellData.getValue().medName());
         medAmountColumn.setCellValueFactory(cellData -> cellData.getValue().medAmount());
     
